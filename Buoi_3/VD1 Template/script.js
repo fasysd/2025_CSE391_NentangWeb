7e9c38ce7c0f4abac7b222865f8f159d2c63a3f1
fasysd <manhhung25017@gmail.com>
2025-03-25 12:20:18 +0700

    Buoi_3

--- Buoi_3/VD1 Template/script.js
+++ Buoi_3/VD1 Template/script.js
@@ -1,14 +1,59 @@
-var toDoList = new Array();
+var toDoList = new Map();
 var taskList = document.getElementById("taskList");
 var taskInput = document.getElementById("taskInput");
 var addBtn = document.getElementById("addBtn");
-console.log(typeof(toDoList))
-addBtn.onclick = () => {
-    toDoList.push( new Array( taskInput.value, false));
+
+var reloadList = () => {
     taskList.innerHTML = "";
-    toDoList.forEach (function(value) {
-        var newChildContent = document.createElement("li");
-        newChildContent.innerHTML = value[0];
-        taskList.appendChild(newChildContent);
+    toDoList.forEach (function(value, key) {
+        var newChild = document.createElement("li");
+        var childContent = document.createElement("div");
+
+        childContent.onclick = () => {
+            value[1] = !value[1];
+            reloadList();
+        }
+        childContent.innerHTML = value[0];
+        childContent.style.color = value[1] ? "grey" : "red";
+
+        var childDeleteButton = document.createElement("button");
+        childDeleteButton.onclick = () => {
+            toDoList.delete( key);
+            console.log( "delete" + key);
+            reloadList();
+        }
+        childDeleteButton.innerHTML = "Xóa";
+
+        newChild.appendChild( childContent);
+        newChild.appendChild( childDeleteButton);
+        taskList.appendChild(newChild);
     })
+}
+
+addBtn.onclick = () => {
+    toDoList.set( new Date().getTime(), new Array( taskInput.value, false));
+    reloadList();
+    // readTextFile( )
+}
+
+var saveTasksToLocalStorage = () => {
+    var reader = new FileReader();
+    reader.readAsDataURL("");
+    var data = require( "./TasksToLocalStorage.json");
+    console.log( data);
+    console.log("---------------")
+}
+var loadTasksFromLocalStorage = () => {
+    
+}
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
 }
